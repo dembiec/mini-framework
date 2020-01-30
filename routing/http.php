a024b025d3803a099f96afc840449ed11e6324f8
Dominik Rembis <dominik.rembis@gmail.com>
2020-01-30 15:27:58 +0100

    router.php: Create the get function and destructor in the class router http.php: call function get

--- routing/http.php
+++ routing/http.php
@@ -1,2 +1,9 @@
 <?php
 use Route\Router;
+$router = new Router;
+
+$router->get('/', 'App\Controllers::Main\Index');
+
+$router->get('galeria', 'App\Controllers::Gallery\Show');
+
+$router->get('kontakt', 'App\Controllers::Contact\Update');
--- routing/router.php
+++ routing/router.php
@@ -3,4 +3,22 @@ namespace Route;
 
 class Router
 {
+    protected $redirectMap = [
+        'get' => [],
+        'post' => []
+    ];
+
+    public function get($url, $tmp)
+    {
+        $this->redirectMap['get'][$url] = $tmp;
+    }
+
+    function __destruct()
+    {
+        if (isset($this->redirectMap['get'][$_GET['url']])) {
+            echo true;
+        } else {
+            echo false;
+        }
+    }
 }
