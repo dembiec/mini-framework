d332e749ea5fcff91ef96d24c1b1e915988c0dff
Dominik Rembis <dominik.rembis@gmail.com>
2020-01-31 18:17:40 +0100

    Update redirects in http.php

--- routing/http.php
+++ routing/http.php
@@ -2,8 +2,4 @@
 use Route\Router;
 $router = new Router;
 
-$router->get('/', 'App\Controllers::Main\Index');
-
-$router->get('galeria', 'App\Controllers::Gallery\Show');
-
-$router->get('kontakt', 'App\Controllers::Contact\Update');
+$router->get('foo', 'Controllers\Foo::bar');
