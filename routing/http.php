c06ac5a76c1616c280793779e2ec8c716b8bed13
Dominik Rembis <dominik.rembis@gmail.com>
2020-02-10 20:05:50 +0100

    Declaring redirects to login methods

--- routing/http.php
+++ routing/http.php
@@ -2,4 +2,13 @@
 use Route\Router;
 $router = new Router;
 
+<<<<<<< Updated upstream
+=======
+$router->get('index', 'Controller\StaticPages::main');
+
+$router->get('login', 'Controller\Auth\Login::index');
+$router->post('login', 'Controller\Auth\Login::login');
+$router->get('logout', 'Controller\Auth\Login::logout');
+
+>>>>>>> Stashed changes
 $router->get('foo/{?}/bar', 'Controller\Foo::bar');
