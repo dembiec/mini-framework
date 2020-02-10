7374a5739742675b6a5ad87b8b0f4fc07385fdf7
Dominik Rembis <dominik.rembis@gmail.com>
2020-02-10 12:58:44 +0100

    Updating method userLogin. Adding to session variable id and email and show in query id, email, password

--- utilities/authorization.php
+++ utilities/authorization.php
@@ -10,9 +10,11 @@ class Authorization
         $password = htmlspecialchars($password);
 
         $database = new Database();
-        $result = $database->query("SELECT password FROM `users` WHERE email = '{$email}'");
+        $result = $database->query("SELECT id, email, password FROM `users` WHERE email = '{$email}'");
 
         if (!empty($result[0]['password']) && password_verify($password, $result[0]['password'])) {
+            $_SESSION['id'] = $result[0]['id'];
+            $_SESSION['email'] = $result[0]['email'];
             return TRUE;
         } else {
             return FALSE;
