7444196b4afc4bf714d6b291f12381e40c29a90c
Dominik Rembis <dominik.rembis@gmail.com>
2020-02-15 16:02:35 +0100

    Adding declaration start page

--- routing/http.php
+++ routing/http.php
@@ -2,13 +2,12 @@
 use Route\Router;
 $router = new Router;
 
-<<<<<<< Updated upstream
-=======
 $router->get('index', 'Controller\StaticPages::main');
 
 $router->get('login', 'Controller\Auth\Login::index');
 $router->post('login', 'Controller\Auth\Login::login');
 $router->get('logout', 'Controller\Auth\Login::logout');
 
->>>>>>> Stashed changes
+$router->get('start', 'Controller\StaticPages::start');
+
 $router->get('foo/{?}/bar', 'Controller\Foo::bar');
