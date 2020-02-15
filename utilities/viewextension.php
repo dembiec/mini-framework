48661712ffb23e49411660e3c87f50725bbe14f2
Dominik Rembis <dominik.rembis@gmail.com>
2020-02-15 12:38:29 +0100

    New function 'isLogged' passed to view

--- utilities/viewextension.php
+++ utilities/viewextension.php
@@ -3,14 +3,18 @@ namespace Utility;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use Utility\Http;
+use Utility\Authorization;
 
 class ViewExtension extends AbstractExtension
 {
     public function getFunctions()
     {
         return [
-            new TwigFunction('appUrl', function($url = NULL) {
+            new TwigFunction('appUrl', function(string $url = NULL) {
                 return Http::appUrl($url); 
+            }),
+            new TwigFunction('isLogged', function() {
+                return Authorization::isLogged(); 
             })
         ];
     }
